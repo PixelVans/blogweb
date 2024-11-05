bd960a485dd8d62f0271da103b55afb2b3ccd6ad
PixelVans <evansmemba001@gmail.com>
2024-11-05 16:27:20 +0300

    header styling complete

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images")
     eleventyConfig.addPassthroughCopy("admin")
+    eleventyConfig.addPassthroughCopy("styles.css")
+    eleventyConfig.addPassthroughCopy("assets")
+    eleventyConfig.addPassthroughCopy("scripts.js")
     };
   ;
+  
   
