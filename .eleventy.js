08273eb242a01af8970557f0e8b894830b55ccf9
PixelVans <evansmemba001@gmail.com>
2024-11-05 22:00:08 +0300

    fotter update and styling

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,21 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("styles.css")
     eleventyConfig.addPassthroughCopy("assets")
     eleventyConfig.addPassthroughCopy("scripts.js")
+
+    
+    eleventyConfig.addFilter("dayOnly", function(date) {
+      const options = { 
+          day: 'numeric', 
+          month: 'long', // Use 'short' for abbreviated month names
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: true // Set to false for 24-hour format
+      };
+      return new Date(date).toLocaleString('en-US', options);
+  });
+  
+  
     };
   ;
   
